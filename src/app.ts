2e201f8c21cc8a58b636230805f60cbd2055d326
ARYAN0-WORK <carrer1270@gmail.com>
2026-07-23 11:52:47 +0000

    feat: intializing app middleware and health endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import helmet from "helmet";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Everything's good");
+app.use(helmet())
+app.use(express.json())
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    success:true,
+    message:`Everything's good`
+  })
 });
 
 export default app;
