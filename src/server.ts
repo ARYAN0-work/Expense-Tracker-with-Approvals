f1b05696357d3838a8c3efdaf66ec750d9d85f25
ARYAN0-WORK <carrer1270@gmail.com>
2026-07-23 14:42:25 +0000

    fix: adding DB connection also improving our server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
 import config from "./config/env.js";
 import app from "./app.js";
+import connectDB from "./db/connection.js";
 
-const PORT = process.env.PORT || 5000;
+const startServer = async () => {
+    try {
+        await connectDB();
 
-app.listen(config.port, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+        app.listen(config.port, () => {
+            console.log(` Server's running on port ${config.port}`);
+        });
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+startServer();
