c1dbbcc3d347563069754104470106b8f1c5c8d9
ARYAN0-WORK <carrer1270@gmail.com>
2026-07-23 14:02:48 +0000

    feat: adding auth route and controller with jwt secret and expiry

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,6 +6,10 @@ const config = {
     port: Number(process.env.PORT) || 5000,
 
     mongoUri: process.env.MONGO_URI || "",
+    
+    jwtSecret: process.env.JWT_SECRET!,
+
+    jwtExpiresIn: process.env.JWT_EXPIRES_IN!,
 };
 
 export default config;
---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+export const register = async (
+  req: Request,
+  res: Response
+)=> {
+  
+
+};
